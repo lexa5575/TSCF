23164d98f7ecf3b53d3e31ee34d566c7277aa3ef
lexa5575 <66919275+lexa5575@users.noreply.github.com>
2023-04-09 13:24:44 +0200

    first com

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::get('/', 'IndexController');
     });
         Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
-            Route::get('/', 'IndexController')->name('admin.category.index');
+            Route::get('/post', 'IndexController')->name('admin.category.index');
             Route::get('/create', 'CreateController')->name('admin.category.create');
             Route::post('/', 'StoreController')->name('admin.category.store');
             Route::get('/{category}', 'ShowController')->name('admin.category.show');
